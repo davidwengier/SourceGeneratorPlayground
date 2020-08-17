b8237da25b09f8bae1a9282eb8552dfffdaddbe4
David Wengier <dawengie@microsoft.com>
2020-08-17 22:29:42 +1000

    Initial commit... after about 4 hours of work :P

---
+++ Samples/Dependency Injection.Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            // var foo = DI.ServiceLocator.GetService<IFoo>();
+
+            // var anotherFoo = DI.ServiceLocator.GetService<IFoo>();
+
+            // var bar = DI.ServiceLocator.GetService<IBar>();
+
+            // var baz = DI.ServiceLocator.GetService<IBaz>();
+
+            Console.WriteLine("Hello World");
+        }
+    }
+
+    interface IFoo
+    {
+    }
+
+    class Foo : IFoo
+    {
+    }
+
+    interface IBar
+    {
+    }
+
+    class Bar : IBar
+    {
+    }
+
+    interface IBaz
+    {
+    }
+}
