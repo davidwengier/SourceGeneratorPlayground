c66e7308466bef64e616267307619cfc24096e74
Stefan Lörwald <10850250+stefanloerwald@users.noreply.github.com>
2021-05-25 09:51:23 +0200

    same as PR#9, except always checking that references are complete

---
+++ IRunner.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace SourceGeneratorPlayground
+{
+    internal interface IRunner
+    {
+        string ErrorText { get; }
+        string GeneratorOutput { get; }
+        string ProgramOutput { get; }
+
+        Task RunAsync(string code, string generator, CancellationToken cancellationToken);
+    }
+}
